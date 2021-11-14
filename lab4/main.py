35729fc4fe1bba5f1ecbcba8aba28d11358ebb35
Artem Buhera <artem.bugera@gmail.com>
2021-11-14 22:40:04 +0100

    balanced simulation

--- lab4/main.py
+++ lab4/main.py
@@ -5,9 +5,9 @@ from PyQt5.QtWidgets import QApplication
 from lab4.visual import GUI
 from simulation import Simulation
 
-sim = Simulation(n=100, m=100, starting_pop_count=75,
-                 grow_count=3, grow_p=0.5)
+sim = Simulation(n=50, m=50, starting_pop_count=100,
+                 grow_count=10, grow_p=0.2)
 
 app = QApplication(sys.argv)
-ex = GUI(sim, turns_per_second=5)
+ex = GUI(sim, turns_per_second=25)
 sys.exit(app.exec_())
