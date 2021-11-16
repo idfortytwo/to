a3997c9fe5a90b65e8bb3d580ff91976cf67df15
Artem Buhera <artem.bugera@gmail.com>
2021-11-16 20:30:38 +0100

    run configurations

--- lab4/main.py
+++ lab4/main.py
@@ -5,13 +5,18 @@ from PyQt5.QtWidgets import QApplication
 from lab4.visual import GUI
 from simulation import Simulation
 
-sim = Simulation(n=50, m=50, starting_pop_count=200,
-                 grow_count=10, grow_p=0.2, immune_p=0.1)
-
-sim2 = Simulation(n=50, m=50, starting_pop_count=200,
-                  grow_count=10, grow_p=0.2, immune_p=0.0)
+params = {
+    'n': 50,
+    'm': 50,
+    'starting_pop_count': 300,
+    'grow_count': 10,
+    'grow_p': 0.3
+}
+sim_no_immune = Simulation(**params, immune_p=0)
+sim_immune = Simulation(**params, immune_p=0.2)
 
 app = QApplication(sys.argv)
-# ex = GUI(sim, turns_per_second=25, preload_turns=50)
-ex = GUI(sim2, turns_per_second=25, preload_turns=300)
+ex = GUI(sim_no_immune, turns_per_second=25, preload_turns=100)
+print('ex1 ready')
+ex2 = GUI(sim_immune, turns_per_second=25, preload_turns=100)
 sys.exit(app.exec_())
